42c15c3e58fbce55d52f2fa4cab7165bf4870122
nanaakuffu <nakuffu@aims.ac.tz>
2021-07-22 17:55:08 +0000

    Added migrations for company and employee table

---
+++ database/migrations/2021_07_22_174240_employee.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Employee extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->foreignId('company')->references('id')->on('company')->onUpdate('cascade');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->text('picture')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("employee");
+    }
+}
